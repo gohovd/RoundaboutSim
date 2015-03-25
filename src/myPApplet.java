9ab40c5efc4243bc5acd029eea97af45d585eb9e
TerjeE <Terje_Eik@hotmail.com>
2015-03-25 12:43:31 +0100

    Added car

--- src/myPApplet.java
+++ src/myPApplet.java
@@ -4,14 +4,21 @@ public class myPApplet extends PApplet {
 
     //public static void main(String args[]) { PApplet.main(new String[]{"--present", "myPApplet"});
     //}
+    PGraphics pg;
+    car testCar;
 
     public void setup(){
         size(500, 500);
         frameRate(60);
-}
+        pg = createGraphics(width,height);
+        testCar = new car(this,20,10,width/2,height/2);
+    }
 
 
     public void draw (){
+        background(200);
+        pg = createGraphics(width,height);
+
         textSize(32);
         fill(255, 0, 0, 255);
         text("8======D", width / 2, height / 2);
@@ -21,5 +28,17 @@ public class myPApplet extends PApplet {
         ellipse(60, 90, 50, 50);
         ellipse(170, 80, 40, 40);
 
+
+
+        testCar.oneStep();
+        testCar.drawSelf(pg);
+        image(pg,0,0);
+
+
+
+    }
+
+    public void setPg(PGraphics pg) {
+        this.pg = pg;
     }
 }
