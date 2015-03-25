adf31afbe0ff931971719ea74dd825a580aba2c9
Brattus <olemartinbratteberg@gmail.com>
2015-03-25 10:50:14 +0100

    Start

---
+++ src/myPApplet.java
@@ -0,0 +1,18 @@
+import processing.core.PApplet;
+
+public class myPApplet extends PApplet {
+
+    public static void main(String args[]) {
+        PApplet.main(new String[]{"--present", "Processing"});
+    }
+
+    public void setup(){
+        size(500, 500);
+        frameRate(60);
+}
+
+
+    public void draw (){
+
+    }
+}
