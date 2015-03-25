abd0c7e98988333c6a22abaf1d96b78ef4fe0087
Brattus <olemartinbratteberg@gmail.com>
2015-03-25 12:43:51 +0100

    Background

--- src/myPApplet.java
+++ src/myPApplet.java
@@ -4,22 +4,49 @@ public class myPApplet extends PApplet {
 
     //public static void main(String args[]) { PApplet.main(new String[]{"--present", "myPApplet"});
     //}
+    PGraphics bg;
+    float roadHeight = 70;
+    float roadWidth = 70;
+    float h = height;
+    float w = width;
 
     public void setup(){
-        size(500, 500);
+        size(1368, 768);
         frameRate(60);
-}
+        bg = createGraphics(1368, 768);
+    }
 
 
-    public void draw (){
-        textSize(32);
-        fill(255, 0, 0, 255);
-        text("8======D", width / 2, height / 2);
+    public void draw () {
+       createBackground();
+    }
 
-        rect(80, 65, 90, 30);
-        ellipse(60, 60, 50, 50);
-        ellipse(60, 90, 50, 50);
-        ellipse(170, 80, 40, 40);
 
+    public void createBackground(){
+        bg.beginDraw();
+        bg.background(240, 240, 240);
+        bg.noStroke();
+        //Roads
+        bg.fill(80);
+            //Horisontal
+        bg.rect(0, height / 2, width * 2, roadHeight);
+            //Vertical
+        bg.rect(width/2,0,roadWidth,height*2);
+        //Yellow line
+        bg.stroke(255,255,0);
+        bg.line(0, h/2, w, h/2);
+        bg.line(width/2,0,width/2,h);
+        bg.line(30, 20, 85, 75);
+        //Outercircle
+        noStroke();
+        bg.fill(80);
+        bg.ellipse(width / 2, height / 2, 300, 300);
+        bg.fill(255);
+        //Innercircle
+        bg.ellipse(width / 2, height / 2, 180, 180);
+        bg.rectMode(CENTER);
+        //End
+        bg.endDraw();
+        image(bg,0,0);
     }
 }
